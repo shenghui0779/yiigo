9c2f84c2305a072c68e6e292ef3f0efd4a28df54
盛辉 <shenghui@lanjingren.com>
2017-12-27 19:38:54 +0800

    add helper

---
+++ helper.go
@@ -0,0 +1,94 @@
+package yiigo
+
+import (
+	"crypto/md5"
+	"fmt"
+	"time"
+)
+
+// MD5 获取字符串md5值
+func MD5(s string) string {
+	h := md5.New()
+	h.Write([]byte(s))
+
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+// Date 时间戳格式化日期
+func Date(timestamp int64, format ...string) string {
+	layout := "2006-01-02 15:04:05"
+
+	if len(format) > 0 {
+		layout = format[0]
+	}
+
+	date := time.Unix(timestamp, 0).Format(layout)
+
+	return date
+}
+
+// IntUnique int切片去重
+func IntUnique(in []int) []int {
+	out := make([]int, 0, len(in))
+
+	for _, i := range in {
+		exist := false
+
+		for _, o := range out {
+			if i == o {
+				exist = true
+				break
+			}
+		}
+
+		if !exist {
+			out = append(out, i)
+		}
+	}
+
+	return out
+}
+
+// Int64Unique int64切片去重
+func Int64Unique(in []int64) []int64 {
+	out := make([]int64, 0, len(in))
+
+	for _, i := range in {
+		exist := false
+
+		for _, o := range out {
+			if i == o {
+				exist = true
+				break
+			}
+		}
+
+		if !exist {
+			out = append(out, i)
+		}
+	}
+
+	return out
+}
+
+// StringUnique string切片去重
+func StringUnique(in []string) []string {
+	out := make([]string, 0, len(in))
+
+	for _, i := range in {
+		exist := false
+
+		for _, o := range out {
+			if i == o {
+				exist = true
+				break
+			}
+		}
+
+		if !exist {
+			out = append(out, i)
+		}
+	}
+
+	return out
+}
--- util.go
+++ util.go
@@ -1,25 +1,14 @@
 package yiigo
 
 import (
-	"crypto/md5"
-	"fmt"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 // X is a convenient alias for a map[string]interface{} map
 type X map[string]interface{}
-
-// MD5 获取字符串md5值
-func MD5(s string) string {
-	h := md5.New()
-	h.Write([]byte(s))
-
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
 
 // IsXhr 判断是否为Ajax请求
 func IsXhr(c *gin.Context) bool {
@@ -30,19 +19,6 @@ func IsXhr(c *gin.Context) bool {
 	}
 
 	return false
-}
-
-// Date 时间戳格式化日期
-func Date(timestamp int64, format ...string) string {
-	layout := "2006-01-02 15:04:05"
-
-	if len(format) > 0 {
-		layout = format[0]
-	}
-
-	date := time.Unix(timestamp, 0).Format(layout)
-
-	return date
 }
 
 // OK API返回成功
