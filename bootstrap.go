c3dc7b5ef8d03da21dc223669bf21f8e4a55d407
IIInsomnia <847713844@qq.com>
2018-06-08 14:03:14 +0800

    add init function to load config

--- bootstrap.go
+++ bootstrap.go
@@ -1,12 +1,14 @@
 package yiigo
 
-// Bootstrap init and start core components of yiigo.
-func Bootstrap(mysql bool, mongo bool, redis bool) error {
+func init() {
 	// load config
 	loadEnv("env.toml")
 	// init logger
 	initLogger()
+}
 
+// Bootstrap init and start core components of yiigo.
+func Bootstrap(mysql bool, mongo bool, redis bool) error {
 	if mysql {
 		// init mysql
 		if err := initMySQL(); err != nil {
