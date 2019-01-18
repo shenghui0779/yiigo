d6a5312683c21e47dd1450de80e80598869d1309
shenghui <shenghui@lanjingren.com>
2019-01-18 11:40:39 +0800

    Improve env

--- env.go
+++ env.go
@@ -3,6 +3,7 @@ package yiigo
 import (
 	"errors"
 	"path/filepath"
+	"reflect"
 	"strconv"
 	"sync"
 	"time"
@@ -23,7 +24,12 @@ var ErrEnvNil = errors.New("yiigo: env config not found")
 
 // loadEnv load env file.
 func loadEnv() {
-	abs, _ := filepath.Abs("env.toml")
+	abs, err := filepath.Abs("env.toml")
+
+	if err != nil {
+		panic(err)
+	}
+
 	tomlTree, err := toml.LoadFile(abs)
 
 	if err != nil {
@@ -41,9 +47,13 @@ func (e *env) String(key string, defaultValue ...string) string {
 		dv = defaultValue[0]
 	}
 
-	i := e.Get(key)
+	ev := e.Get(key)
 
-	switch t := i.(type) {
+	if ev == nil {
+		return dv
+	}
+
+	switch t := ev.(type) {
 	case string:
 		return t
 	case int64:
@@ -55,8 +65,50 @@ func (e *env) String(key string, defaultValue ...string) string {
 	case bool:
 		return strconv.FormatBool(t)
 	default:
-		return dv
+		return ""
 	}
+}
+
+// Strings returns a value of []string.
+func (e *env) Strings(key string, defaultValue ...string) []string {
+	ev := e.Get(key)
+
+	if ev == nil {
+		return defaultValue
+	}
+
+	v := reflect.Indirect(reflect.ValueOf(ev))
+
+	if v.Kind() != reflect.Slice {
+		return []string{}
+	}
+
+	l := v.Len()
+
+	result := make([]string, 0, l)
+
+	if l == 0 {
+		return result
+	}
+
+	for i := 0; i < l; i++ {
+		switch t := v.Index(i).Interface().(type) {
+		case string:
+			result = append(result, t)
+		case int64:
+			result = append(result, strconv.FormatInt(t, 10))
+		case uint64:
+			result = append(result, strconv.FormatInt(int64(t), 10))
+		case float64:
+			result = append(result, strconv.FormatFloat(t, 'f', -1, 64))
+		case bool:
+			result = append(result, strconv.FormatBool(t))
+		default:
+			result = append(result, "")
+		}
+	}
+
+	return result
 }
 
 // Int returns a value of int.
@@ -67,9 +119,13 @@ func (e *env) Int(key string, defaultValue ...int) int {
 		dv = defaultValue[0]
 	}
 
-	i := e.Get(key)
+	ev := e.Get(key)
 
-	switch t := i.(type) {
+	if ev == nil {
+		return dv
+	}
+
+	switch t := ev.(type) {
 	case int64:
 		return int(t)
 	case uint64:
@@ -77,11 +133,9 @@ func (e *env) Int(key string, defaultValue ...int) int {
 	case float64:
 		return int(t)
 	case string:
-		if v, err := strconv.ParseInt(t, 0, 0); err == nil {
-			return int(v)
-		}
+		v, _ := strconv.Atoi(t)
 
-		return dv
+		return v
 	case bool:
 		if t {
 			return 1
@@ -89,8 +143,55 @@ func (e *env) Int(key string, defaultValue ...int) int {
 
 		return 0
 	default:
-		return dv
+		return 0
 	}
+}
+
+// Ints returns a value of []int.
+func (e *env) Ints(key string, defaultValue ...int) []int {
+	ev := e.Get(key)
+
+	if ev == nil {
+		return []int{}
+	}
+
+	v := reflect.Indirect(reflect.ValueOf(ev))
+
+	if v.Kind() != reflect.Slice {
+		return defaultValue
+	}
+
+	l := v.Len()
+
+	result := make([]int, 0, l)
+
+	if l == 0 {
+		return result
+	}
+
+	for i := 0; i < l; i++ {
+		switch t := v.Index(i).Interface().(type) {
+		case int64:
+			result = append(result, int(t))
+		case uint64:
+			result = append(result, int(t))
+		case float64:
+			result = append(result, int(t))
+		case string:
+			n, _ := strconv.Atoi(t)
+			result = append(result, n)
+		case bool:
+			if t {
+				result = append(result, 1)
+			} else {
+				result = append(result, 0)
+			}
+		default:
+			result = append(result, 0)
+		}
+	}
+
+	return result
 }
 
 // Int64 returns a value of int64.
@@ -101,9 +202,13 @@ func (e *env) Int64(key string, defaultValue ...int64) int64 {
 		dv = defaultValue[0]
 	}
 
-	i := e.Get(key)
+	ev := e.Get(key)
 
-	switch t := i.(type) {
+	if ev == nil {
+		return dv
+	}
+
+	switch t := ev.(type) {
 	case int64:
 		return t
 	case uint64:
@@ -111,11 +216,9 @@ func (e *env) Int64(key string, defaultValue ...int64) int64 {
 	case float64:
 		return int64(t)
 	case string:
-		if v, err := strconv.ParseInt(t, 0, 0); err == nil {
-			return v
-		}
+		v, _ := strconv.ParseInt(t, 0, 0)
 
-		return dv
+		return v
 	case bool:
 		if t {
 			return 1
@@ -123,8 +226,56 @@ func (e *env) Int64(key string, defaultValue ...int64) int64 {
 
 		return 0
 	default:
-		return dv
+		return 0
 	}
+}
+
+// Int64s returns a value of []int64.
+func (e *env) Int64s(key string, defaultValue ...int64) []int64 {
+	ev := e.Get(key)
+
+	if ev == nil {
+		return defaultValue
+	}
+
+	v := reflect.Indirect(reflect.ValueOf(ev))
+
+	if v.Kind() != reflect.Slice {
+		return []int64{}
+	}
+
+	l := v.Len()
+
+	result := make([]int64, 0, l)
+
+	if l == 0 {
+		return result
+	}
+
+	for i := 0; i < l; i++ {
+		switch t := v.Index(i).Interface().(type) {
+		case int64:
+			result = append(result, t)
+		case uint64:
+			result = append(result, int64(t))
+		case float64:
+			result = append(result, int64(t))
+		case string:
+			n, _ := strconv.ParseInt(t, 10, 64)
+
+			result = append(result, n)
+		case bool:
+			if t {
+				result = append(result, 1)
+			} else {
+				result = append(result, 0)
+			}
+		default:
+			result = append(result, 0)
+		}
+	}
+
+	return result
 }
 
 // Float64 returns a value of float64.
@@ -135,9 +286,13 @@ func (e *env) Float64(key string, defaultValue ...float64) float64 {
 		dv = defaultValue[0]
 	}
 
-	i := e.Get(key)
+	ev := e.Get(key)
 
-	switch t := i.(type) {
+	if ev == nil {
+		return dv
+	}
+
+	switch t := ev.(type) {
 	case float64:
 		return t
 	case int64:
@@ -145,11 +300,9 @@ func (e *env) Float64(key string, defaultValue ...float64) float64 {
 	case uint64:
 		return float64(t)
 	case string:
-		if v, err := strconv.ParseFloat(t, 64); err == nil {
-			return v
-		}
+		v, _ := strconv.ParseFloat(t, 64)
 
-		return dv
+		return v
 	case bool:
 		if t {
 			return 1
@@ -157,8 +310,56 @@ func (e *env) Float64(key string, defaultValue ...float64) float64 {
 
 		return 0
 	default:
-		return dv
+		return 0
 	}
+}
+
+// Float64s returns a value of []float64.
+func (e *env) Float64s(key string, defaultValue ...float64) []float64 {
+	ev := e.Get(key)
+
+	if ev == nil {
+		return defaultValue
+	}
+
+	v := reflect.Indirect(reflect.ValueOf(ev))
+
+	if v.Kind() != reflect.Slice {
+		return []float64{}
+	}
+
+	l := v.Len()
+
+	result := make([]float64, 0, l)
+
+	if l == 0 {
+		return result
+	}
+
+	for i := 0; i < l; i++ {
+		switch t := v.Index(i).Interface().(type) {
+		case float64:
+			result = append(result, t)
+		case int64:
+			result = append(result, float64(t))
+		case uint64:
+			result = append(result, float64(t))
+		case string:
+			n, _ := strconv.ParseFloat(t, 64)
+
+			result = append(result, n)
+		case bool:
+			if t {
+				result = append(result, 1)
+			} else {
+				result = append(result, 0)
+			}
+		default:
+			result = append(result, 0)
+		}
+	}
+
+	return result
 }
 
 // Bool returns a value of bool.
@@ -169,9 +370,13 @@ func (e *env) Bool(key string, defaultValue ...bool) bool {
 		dv = defaultValue[0]
 	}
 
-	i := e.Get(key)
+	ev := e.Get(key)
 
-	switch t := i.(type) {
+	if ev == nil {
+		return dv
+	}
+
+	switch t := ev.(type) {
 	case bool:
 		return t
 	case int64:
@@ -187,51 +392,50 @@ func (e *env) Bool(key string, defaultValue ...bool) bool {
 
 		return false
 	case string:
-		if v, err := strconv.ParseBool(t); err == nil {
-			return v
-		}
+		v, _ := strconv.ParseBool(t)
 
-		return dv
+		return v
 	default:
-		return dv
+		return false
 	}
 }
 
 // Time returns a value of time.Time.
-func (e *env) Time(key string, defaultValue ...time.Time) time.Time {
+// Layout is required when the env value is a string.
+func (e *env) Time(key string, layout string, defaultValue ...time.Time) time.Time {
 	var dv time.Time
 
 	if len(defaultValue) > 0 {
 		dv = defaultValue[0]
 	}
 
-	i := e.Get(key)
+	ev := e.Get(key)
 
-	switch t := i.(type) {
+	if ev == nil {
+		return dv
+	}
+
+	switch t := ev.(type) {
 	case time.Time:
 		return t
 	case string:
-		if v, err := time.Parse("2006-01-02 15:04:05", t); err == nil {
-			return v
-		}
+		v, _ := time.Parse(layout, t)
 
-		return dv
+		return v
 	case int64:
 		return time.Unix(t, 0)
 	case uint64:
 		return time.Unix(int64(t), 0)
 	default:
-		return dv
+		return time.Time{}
 	}
 }
 
 // Map returns a value of map[string]interface{}.
 func (e *env) Map(key string) map[string]interface{} {
-	i := e.Get(key)
-
 	m := make(map[string]interface{})
 
-	if v, ok := i.(*toml.Tree); ok {
+	if v, ok := e.Get(key).(*toml.Tree); ok {
 		m = v.ToMap()
 	}
 
@@ -255,7 +459,7 @@ func (e *env) Unmarshal(key string, dest interface{}) error {
 	return errors.New("yiigo: env config is not a tree of toml")
 }
 
-// Get returns a value of interface{}.
+// Get returns the value at key in the env Tree.
 func (e *env) Get(key string) interface{} {
 	e.mutex.RLock()
 	defer e.mutex.RUnlock()
