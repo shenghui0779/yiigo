6e2e742f6f0aff21ff4a172ec4da850d4fb5c04a
shenghui <shenghui@lanjingren.com>
2019-04-19 18:02:06 +0800

    update PKCS#7

--- crypto.go
+++ crypto.go
@@ -45,21 +45,30 @@ func AESCBCDecrypt(cipherText, key []byte, iv ...byte) ([]byte, error) {
 	blockMode := cipher.NewCBCDecrypter(block, iv)
 	blockMode.CryptBlocks(plainText, cipherText)
 
-	return PKCS7UnPadding(plainText), nil
+	return PKCS7UnPadding(plainText, len(key)), nil
 }
 
 // PKCS7Padding PKCS7 padding
 func PKCS7Padding(cipherText []byte, blockSize int) []byte {
 	padding := blockSize - len(cipherText)%blockSize
+
+	if padding == 0 {
+		padding = blockSize
+	}
+
 	padText := bytes.Repeat([]byte{byte(padding)}, padding)
 
 	return append(cipherText, padText...)
 }
 
 // PKCS7UnPadding PKCS7 unpadding
-func PKCS7UnPadding(decryptedData []byte) []byte {
-	l := len(decryptedData)
-	unpadding := int(decryptedData[l-1])
+func PKCS7UnPadding(plainText []byte, blockSize int) []byte {
+	l := len(plainText)
+	unpadding := int(plainText[l-1])
 
-	return decryptedData[:(l - unpadding)]
+	if unpadding < 1 || unpadding > blockSize {
+		unpadding = 0
+	}
+
+	return plainText[:(l - unpadding)]
 }
