0e753bd196868daab4e326f50675fe1421f7c0a5
shenghui <shenghui@lanjingren.com>
2020-09-10 14:22:55 +0800

    update init.go

--- init.go
+++ init.go
@@ -5,7 +5,7 @@ var debug bool
 func init() {
 	// init default logger
 	logger = newLogger(&logConfig{
-		Path:       "app.log",
+		Path:       "logs/app.log",
 		MaxSize:    500,
 		MaxBackups: 0,
 		MaxAge:     0,
