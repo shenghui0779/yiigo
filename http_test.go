00ce0afa757f75d6fe959dfe273fe5ad5fd4e6d6
shenghui <shenghui@lanjingren.com>
2020-11-17 16:10:56 +0800

    add http test

---
+++ http_test.go
@@ -0,0 +1,33 @@
+package yiigo
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHTTPOption(t *testing.T) {
+	options := []HTTPOption{
+		WithHTTPHeader("Accept-Language", "zh-CN,zh;q=0.9"),
+		WithHTTPHeader("Content-Type", "text/xml; charset=utf-8"),
+		WithHTTPClose(),
+		WithHTTPTimeout(5 * time.Second),
+	}
+
+	o := &httpOptions{
+		headers: make(map[string]string),
+		timeout: defaultHTTPTimeout,
+	}
+
+	for _, option := range options {
+		option.apply(o)
+	}
+
+	assert.Equal(t, map[string]string{
+		"Accept-Language": "zh-CN,zh;q=0.9",
+		"Content-Type":    "text/xml; charset=utf-8",
+	}, o.headers)
+	assert.Equal(t, true, o.close)
+	assert.Equal(t, 5*time.Second, o.timeout)
+}
