25b4bc4f660cb75467f81503957c511e014c2a8b
shenghui <shenghui0779@gmail.com>
2023-07-07 10:33:59 +0800

    更新values

--- values.go
+++ values.go
@@ -6,26 +6,26 @@ import (
 	"strings"
 )
 
-// Values 用于处理 k-v 需要格式化的场景，如：签名
-type Values map[string]string
+// V 用于处理 k-v 需要格式化的场景，如：签名
+type V map[string]string
 
 // Set 设置 k-v
-func (v Values) Set(key, value string) {
+func (v V) Set(key, value string) {
 	v[key] = value
 }
 
 // Get 获取值
-func (v Values) Get(key string) string {
+func (v V) Get(key string) string {
 	return v[key]
 }
 
 // Del 删除Key
-func (v Values) Del(key string) {
+func (v V) Del(key string) {
 	delete(v, key)
 }
 
 // Has 判断Key是否存在
-func (v Values) Has(key string) bool {
+func (v V) Has(key string) bool {
 	_, ok := v[key]
 
 	return ok
@@ -34,9 +34,15 @@ func (v Values) Has(key string) bool {
 // Encode 通过自定义的符号和分隔符按照key的ASCII码升序格式化为字符串。
 // 例如：("=", "&") ---> bar=baz&foo=quux；
 // 例如：(":", "#") ---> bar:baz#foo:quux；
-func (v Values) Encode(sym, sep string) string {
+func (v V) Encode(sym, sep string, options ...VEncodeOption) string {
 	if len(v) == 0 {
 		return ""
+	}
+
+	setting := new(vencodeSetting)
+
+	for _, f := range options {
+		f(setting)
 	}
 
 	keys := make([]string, 0, len(v))
@@ -50,46 +56,70 @@ func (v Values) Encode(sym, sep string) string {
 	var buf strings.Builder
 
 	for _, k := range keys {
+		val := v[k]
+
+		if len(val) == 0 && setting.emptyMode == EmptyEncodeIgnore {
+			continue
+		}
+
 		if buf.Len() > 0 {
 			buf.WriteString(sep)
 		}
 
-		buf.WriteString(k)
-		buf.WriteString(sym)
-		buf.WriteString(v[k])
+		if setting.escape {
+			buf.WriteString(url.QueryEscape(k))
+		} else {
+			buf.WriteString(k)
+		}
+
+		if len(val) != 0 {
+			buf.WriteString(sym)
+
+			if setting.escape {
+				buf.WriteString(url.QueryEscape(val))
+			} else {
+				buf.WriteString(val)
+			}
+
+			continue
+		}
+
+		// 保留符号
+		if setting.emptyMode != EmptyEncodeOnlyKey {
+			buf.WriteString(sym)
+		}
 	}
 
 	return buf.String()
 }
 
-// EncodeEscape 通过自定义的符号和分隔符按照key的ASCII码升序格式化为字符串。
-// 例如：("=", "&") ---> bar=baz&foo=quux；
-// 例如：(":", "#") ---> bar:baz#foo:quux；
-// 注意：这里 key 和 value 会被 QueryEscape；
-func (v Values) EncodeEscape(sym, sep string) string {
-	if v == nil {
-		return ""
+// VEmptyEncodeMode 值为空时的Encode模式
+type VEmptyEncodeMode int
+
+const (
+	EmptyEncodeDefault VEmptyEncodeMode = iota // 默认：bar=baz&foo=
+	EmptyEncodeIgnore                          // 忽略：bar=baz
+	EmptyEncodeOnlyKey                         // 仅保留Key：bar=baz&foo
+)
+
+type vencodeSetting struct {
+	escape    bool
+	emptyMode VEmptyEncodeMode
+}
+
+// VEncodeOption V Encode 选项
+type VEncodeOption func(s *vencodeSetting)
+
+// WithEmptyEncodeMode 设置值为空时的Encode模式
+func WithEmptyEncodeMode(mode VEmptyEncodeMode) VEncodeOption {
+	return func(s *vencodeSetting) {
+		s.emptyMode = mode
 	}
+}
 
-	keys := make([]string, 0, len(v))
-
-	for k := range v {
-		keys = append(keys, k)
+// WithKVEscape 设置K-V是否需要QueryEscape
+func WithKVEscape() VEncodeOption {
+	return func(s *vencodeSetting) {
+		s.escape = true
 	}
-
-	sort.Strings(keys)
-
-	var buf strings.Builder
-
-	for _, k := range keys {
-		if buf.Len() > 0 {
-			buf.WriteString(sep)
-		}
-
-		buf.WriteString(url.QueryEscape(k))
-		buf.WriteString(sym)
-		buf.WriteString(url.QueryEscape(v[k]))
-	}
-
-	return buf.String()
 }
--- values_test.go
+++ values_test.go
@@ -6,8 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValues(t *testing.T) {
-	v1 := Values{}
+func TestV(t *testing.T) {
+	v1 := V{}
 
 	v1.Set("bar", "baz")
 	v1.Set("foo", "quux")
@@ -15,11 +15,21 @@ func TestValues(t *testing.T) {
 	assert.Equal(t, "bar=baz&foo=quux", v1.Encode("=", "&"))
 	assert.Equal(t, "bar:baz#foo:quux", v1.Encode(":", "#"))
 
-	v2 := Values{}
+	v2 := V{}
 
 	v2.Set("bar", "baz@666")
 	v2.Set("foo", "quux%666")
 
-	assert.Equal(t, "bar=baz%40666&foo=quux%25666", v2.EncodeEscape("=", "&"))
-	assert.Equal(t, "bar:baz%40666#foo:quux%25666", v2.EncodeEscape(":", "#"))
+	assert.Equal(t, "bar=baz%40666&foo=quux%25666", v2.Encode("=", "&", WithKVEscape()))
+	assert.Equal(t, "bar:baz%40666#foo:quux%25666", v2.Encode(":", "#", WithKVEscape()))
+
+	v3 := V{}
+
+	v3.Set("bar", "baz")
+	v3.Set("foo", "")
+
+	assert.Equal(t, "bar=baz&foo=", v3.Encode("=", "&"))
+	assert.Equal(t, "bar=baz&foo=", v3.Encode("=", "&", WithEmptyEncodeMode(EmptyEncodeDefault)))
+	assert.Equal(t, "bar=baz&foo", v3.Encode("=", "&", WithEmptyEncodeMode(EmptyEncodeOnlyKey)))
+	assert.Equal(t, "bar=baz", v3.Encode("=", "&", WithEmptyEncodeMode(EmptyEncodeIgnore)))
 }
