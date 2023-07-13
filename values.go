1b1cc9c5c15867a2fd22b90b01d080b30d42f4a1
shenghui <shenghui0779@gmail.com>
2023-07-13 19:08:24 +0800

    Update values.go

--- values.go
+++ values.go
@@ -1,6 +1,9 @@
 package yiigo
 
 import (
+	"bytes"
+	"encoding/xml"
+	"io"
 	"net/url"
 	"sort"
 	"strings"
@@ -34,12 +37,14 @@ func (v V) Has(key string) bool {
 // Encode 通过自定义的符号和分隔符按照key的ASCII码升序格式化为字符串。
 // 例如：("=", "&") ---> bar=baz&foo=quux；
 // 例如：(":", "#") ---> bar:baz#foo:quux；
-func (v V) Encode(sym, sep string, options ...VEncodeOption) string {
+func (v V) Encode(sym, sep string, options ...EncodeVOption) string {
 	if len(v) == 0 {
 		return ""
 	}
 
-	setting := new(vencodeSetting)
+	setting := &encodeVSetting{
+		ignoreKeys: make(map[string]struct{}),
+	}
 
 	for _, f := range options {
 		f(setting)
@@ -48,7 +53,9 @@ func (v V) Encode(sym, sep string, options ...VEncodeOption) string {
 	keys := make([]string, 0, len(v))
 
 	for k := range v {
-		keys = append(keys, k)
+		if _, ok := setting.ignoreKeys[k]; !ok {
+			keys = append(keys, k)
+		}
 	}
 
 	sort.Strings(keys)
@@ -102,24 +109,113 @@ const (
 	EmptyEncodeOnlyKey                         // 仅保留Key：bar=baz&foo
 )
 
-type vencodeSetting struct {
-	escape    bool
-	emptyMode VEmptyEncodeMode
+type encodeVSetting struct {
+	escape     bool
+	emptyMode  VEmptyEncodeMode
+	ignoreKeys map[string]struct{}
 }
 
-// VEncodeOption V Encode 选项
-type VEncodeOption func(s *vencodeSetting)
+// EncodeVOption V Encode 选项
+type EncodeVOption func(s *encodeVSetting)
 
 // WithEmptyEncodeMode 设置值为空时的Encode模式
-func WithEmptyEncodeMode(mode VEmptyEncodeMode) VEncodeOption {
-	return func(s *vencodeSetting) {
+func WithEmptyEncodeMode(mode VEmptyEncodeMode) EncodeVOption {
+	return func(s *encodeVSetting) {
 		s.emptyMode = mode
 	}
 }
 
 // WithKVEscape 设置K-V是否需要QueryEscape
-func WithKVEscape() VEncodeOption {
-	return func(s *vencodeSetting) {
+func WithKVEscape() EncodeVOption {
+	return func(s *encodeVSetting) {
 		s.escape = true
+	}
+}
+
+// WithIgnoreKeys 设置Encode时忽略的key
+func WithIgnoreKeys(keys ...string) EncodeVOption {
+	return func(s *encodeVSetting) {
+		for _, k := range keys {
+			s.ignoreKeys[k] = struct{}{}
+		}
+	}
+}
+
+// FormatVToXML format map to xml
+func FormatVToXML(vals V) ([]byte, error) {
+	var builder strings.Builder
+
+	builder.WriteString("<xml>")
+
+	for k, v := range vals {
+		builder.WriteString("<" + k + ">")
+
+		if err := xml.EscapeText(&builder, []byte(v)); err != nil {
+			return nil, err
+		}
+
+		builder.WriteString("</" + k + ">")
+	}
+
+	builder.WriteString("</xml>")
+
+	return []byte(builder.String()), nil
+}
+
+// ParseXMLToV parse xml to map
+func ParseXMLToV(b []byte) (V, error) {
+	m := make(V)
+
+	xmlReader := bytes.NewReader(b)
+
+	var (
+		d     = xml.NewDecoder(xmlReader)
+		tk    xml.Token
+		depth = 0 // current xml.Token depth
+		key   string
+		buf   bytes.Buffer
+		err   error
+	)
+
+	d.Strict = false
+
+	for {
+		tk, err = d.Token()
+
+		if err != nil {
+			if err == io.EOF {
+				return m, nil
+			}
+
+			return nil, err
+		}
+
+		switch v := tk.(type) {
+		case xml.StartElement:
+			depth++
+
+			switch depth {
+			case 2:
+				key = v.Name.Local
+				buf.Reset()
+			case 3:
+				if err = d.Skip(); err != nil {
+					return nil, err
+				}
+
+				depth--
+				key = "" // key == "" indicates that the node with depth==2 has children
+			}
+		case xml.CharData:
+			if depth == 2 && key != "" {
+				buf.Write(v)
+			}
+		case xml.EndElement:
+			if depth == 2 && key != "" {
+				m[key] = buf.String()
+			}
+
+			depth--
+		}
 	}
 }
--- values_test.go
+++ values_test.go
@@ -20,16 +20,39 @@ func TestV(t *testing.T) {
 	v2.Set("bar", "baz@666")
 	v2.Set("foo", "quux%666")
 
+	assert.Equal(t, "bar=baz@666&foo=quux%666", v2.Encode("=", "&"))
 	assert.Equal(t, "bar=baz%40666&foo=quux%25666", v2.Encode("=", "&", WithKVEscape()))
-	assert.Equal(t, "bar:baz%40666#foo:quux%25666", v2.Encode(":", "#", WithKVEscape()))
 
 	v3 := V{}
 
+	v3.Set("hello", "world")
 	v3.Set("bar", "baz")
 	v3.Set("foo", "")
 
-	assert.Equal(t, "bar=baz&foo=", v3.Encode("=", "&"))
-	assert.Equal(t, "bar=baz&foo=", v3.Encode("=", "&", WithEmptyEncodeMode(EmptyEncodeDefault)))
-	assert.Equal(t, "bar=baz&foo", v3.Encode("=", "&", WithEmptyEncodeMode(EmptyEncodeOnlyKey)))
-	assert.Equal(t, "bar=baz", v3.Encode("=", "&", WithEmptyEncodeMode(EmptyEncodeIgnore)))
+	assert.Equal(t, "bar=baz&foo=&hello=world", v3.Encode("=", "&"))
+	assert.Equal(t, "bar=baz&foo=&hello=world", v3.Encode("=", "&", WithEmptyEncodeMode(EmptyEncodeDefault)))
+	assert.Equal(t, "bar=baz&foo&hello=world", v3.Encode("=", "&", WithEmptyEncodeMode(EmptyEncodeOnlyKey)))
+	assert.Equal(t, "bar=baz&hello=world", v3.Encode("=", "&", WithEmptyEncodeMode(EmptyEncodeIgnore)))
+	assert.Equal(t, "bar=baz&foo=", v3.Encode("=", "&", WithIgnoreKeys("hello")))
+	assert.Equal(t, "bar=baz", v3.Encode("=", "&", WithIgnoreKeys("hello"), WithEmptyEncodeMode(EmptyEncodeIgnore)))
+}
+
+func TestXML(t *testing.T) {
+	m := V{
+		"appid":     "wx2421b1c4370ec43b",
+		"partnerid": "10000100",
+		"prepayid":  "WX1217752501201407033233368018",
+		"package":   "Sign=WXPay",
+		"noncestr":  "5K8264ILTKCH16CQ2502SI8ZNMTM67VS",
+		"timestamp": "1514363815",
+	}
+
+	x, err := FormatVToXML(m)
+
+	assert.Nil(t, err)
+
+	r, err := ParseXMLToV([]byte(x))
+
+	assert.Nil(t, err)
+	assert.Equal(t, m, r)
 }
