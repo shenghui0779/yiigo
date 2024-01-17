d685b8d2713bfd142696823a76c85fbbf9178ec8
shenghui <shenghui0779@gmail.com>
2024-01-17 20:10:33 +0800

    Update websocket.go

--- go.mod
+++ go.mod
@@ -6,19 +6,20 @@ require (
 	github.com/fsnotify/fsnotify v1.7.0
 	github.com/go-playground/locales v0.14.1
 	github.com/go-playground/universal-translator v0.18.1
-	github.com/go-playground/validator/v10 v10.16.0
+	github.com/go-playground/validator/v10 v10.17.0
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/gorilla/websocket v1.5.1
 	github.com/hashicorp/go-version v1.6.0
-	github.com/jackc/pgx/v5 v5.5.1
+	github.com/jackc/pgx/v5 v5.5.2
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.19
 	github.com/nsqio/go-nsq v1.1.0
-	github.com/redis/go-redis/v9 v9.3.1
+	github.com/redis/go-redis/v9 v9.4.0
 	github.com/stretchr/testify v1.8.4
 	go.uber.org/zap v1.26.0
-	golang.org/x/crypto v0.17.0
+	golang.org/x/crypto v0.18.0
+	golang.org/x/sync v0.6.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
 
@@ -35,9 +36,8 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sync v0.5.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- websocket.go
+++ websocket.go
@@ -3,12 +3,15 @@ package yiigo
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
+	"runtime/debug"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"go.uber.org/zap"
+	"golang.org/x/sync/singleflight"
 )
-
-var upgrader *websocket.Upgrader
 
 // WSMessage websocket消息
 type WSMessage struct {
@@ -24,86 +27,276 @@ func (m *WSMessage) V() []byte {
 	return m.v
 }
 
-// NewWSMessage 返回一个websocket消息
-func NewWSMessage(t int, v []byte) *WSMessage {
+func NewWSMessage(t int, b []byte) *WSMessage {
 	return &WSMessage{
 		t: t,
-		v: v,
+		v: b,
 	}
 }
 
-// NewWSTextMsg 返回一个websocket.TextMessage
-func NewWSTextMsg(s string) *WSMessage {
+func WSTextMessage(s string) *WSMessage {
 	return &WSMessage{
 		t: websocket.TextMessage,
 		v: []byte(s),
 	}
 }
 
-// NewWSBinaryMsg 返回一个websocket.BinaryMessage
-func NewWSBinaryMsg(v []byte) *WSMessage {
+func WSBinaryMessage(b []byte) *WSMessage {
 	return &WSMessage{
 		t: websocket.BinaryMessage,
-		v: v,
+		v: b,
 	}
 }
 
-// WSConn websocket连接
-type WSConn interface {
-	// Read 读消息
-	Read(ctx context.Context, handler func(ctx context.Context, msg *WSMessage) (*WSMessage, error)) error
-	// Write 写消息
-	Write(ctx context.Context, msg *WSMessage) error
-	// Close 关闭连接
-	Close(ctx context.Context) error
+func WSCloseMessage(code int, text string) *WSMessage {
+	return &WSMessage{
+		t: websocket.PingMessage,
+		v: websocket.FormatCloseMessage(code, text),
+	}
 }
 
-type wsconn struct {
+func WSPingMessage(b []byte) *WSMessage {
+	return &WSMessage{
+		t: websocket.PingMessage,
+		v: b,
+	}
+}
+
+func WSPongMessage(b []byte) *WSMessage {
+	return &WSMessage{
+		t: websocket.PongMessage,
+		v: b,
+	}
+}
+
+// ----------------------------------------- dialer -----------------------------------------
+
+var dialer = websocket.DefaultDialer
+
+// SetDialer 设置 websocket dialer
+func SetDialer(d *websocket.Dialer) {
+	dialer = d
+}
+
+// DialConn websocket拨号连接，支持(读/写)失败重连
+type DialConn struct {
+	key    string
+	addr   string
+	header http.Header
 	conn   *websocket.Conn
-	authOK bool
-	authFn func(ctx context.Context, msg *WSMessage) (*WSMessage, error)
+	mutex  singleflight.Group
 }
 
-func (c *wsconn) Read(ctx context.Context, handler func(ctx context.Context, msg *WSMessage) (*WSMessage, error)) error {
+func (c *DialConn) reconnect() error {
+	// 并发安全，多次请求只会重连一次
+	ch := c.mutex.DoChan(c.key, func() (interface{}, error) {
+		// 创建新连接
+		conn, _, err := dialer.Dial(c.addr, c.header)
+		if err != nil {
+			return false, err
+		}
+
+		// 关闭原连接
+		c.conn.Close()
+		// 设置新连接
+		c.conn = conn
+		// 5秒后清除缓存
+		go func() {
+			time.Sleep(5 * time.Second)
+			c.mutex.Forget(c.key)
+		}()
+
+		return true, nil
+	})
+
+	// 设置超时，以防阻塞
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var err error
+	select {
+	case <-ctx.Done(): // 超时
+		err = ctx.Err()
+	case r := <-ch:
+		err = r.Err
+	}
+	if err != nil {
+		// 重连失败，清除缓存
+		c.mutex.Forget(c.key)
+	}
+
+	return err
+}
+
+// ReadMessage 读取消息，若失败会尝试重连 (reconnectTimeout<=0 表示重连不超时)
+func (c *DialConn) Read(reconnectTimeout time.Duration, handler func(msg *WSMessage)) error {
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Error("websocket read panic", zap.Any("error", err), zap.ByteString("stack", debug.Stack()))
+		}
+	}()
+
 	for {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		default:
-			t, b, err := c.conn.ReadMessage()
-			if err != nil {
-				return err
+		t, b, err := c.conn.ReadMessage()
+		if err == nil {
+			handler(NewWSMessage(t, b))
+			continue
+		}
+
+		var cancel context.CancelFunc
+
+		ctx := context.Background()
+		if reconnectTimeout > 0 {
+			ctx, cancel = context.WithTimeout(ctx, reconnectTimeout)
+		}
+
+		// 尝试重连
+		for {
+			select {
+			case <-ctx.Done():
+				if cancel != nil {
+					cancel()
+				}
+				return fmt.Errorf("websocket reconnect timeout: %w", ctx.Err())
+			default:
 			}
 
-			var msg *WSMessage
-
-			// if `authFunc` is not nil and unauthorized, need to authorize first.
-			if c.authFn != nil && !c.authOK {
-				msg, err = c.authFn(ctx, NewWSMessage(t, b))
-				if err != nil {
-					msg = NewWSTextMsg(err.Error())
-				} else {
-					c.authOK = true
+			// 1秒后尝试重连
+			time.Sleep(time.Second)
+			if err = c.reconnect(); err == nil {
+				if cancel != nil {
+					cancel()
 				}
-			} else {
-				if handler != nil {
-					msg, err = handler(ctx, NewWSMessage(t, b))
-					if err != nil {
-						msg = NewWSTextMsg(err.Error())
-					}
-				}
-			}
-
-			if msg != nil {
-				if err = c.conn.WriteMessage(msg.T(), msg.V()); err != nil {
-					return err
-				}
+				break
 			}
 		}
 	}
 }
 
-func (c *wsconn) Write(ctx context.Context, msg *WSMessage) error {
+// WriteMessage 写入消息，若失败会尝试重连 (reconnectTimeout<=0 表示重连不超时)
+func (c *DialConn) Write(reconnectTimeout time.Duration, msg *WSMessage) error {
+	for {
+		err := c.conn.WriteMessage(msg.T(), msg.V())
+		if err == nil {
+			return nil
+		}
+
+		var cancel context.CancelFunc
+
+		ctx := context.Background()
+		if reconnectTimeout > 0 {
+			ctx, cancel = context.WithTimeout(ctx, reconnectTimeout)
+		}
+
+		// 尝试重连
+		for {
+			select {
+			case <-ctx.Done():
+				if cancel != nil {
+					cancel()
+				}
+				return fmt.Errorf("websocket reconnect timeout: %w", ctx.Err())
+			default:
+			}
+
+			// 1秒后尝试重连
+			time.Sleep(time.Second)
+			if err = c.reconnect(); err == nil {
+				if cancel != nil {
+					cancel()
+				}
+				break
+			}
+		}
+	}
+}
+
+func (c *DialConn) Close() error {
+	return c.conn.Close()
+}
+
+// WSDial 创建一个websocket拨号连接
+func WSDial(ctx context.Context, addr string, header http.Header) (*DialConn, error) {
+	conn, _, err := dialer.DialContext(ctx, addr, header)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DialConn{
+		key:    MD5(addr),
+		addr:   addr,
+		header: header,
+		conn:   conn,
+	}, nil
+}
+
+// ----------------------------------------- upgrader -----------------------------------------
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:    4096,
+	WriteBufferSize:   4096,
+	EnableCompression: true,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// SetUpgrader 设置 websocket Upgrader
+func SetUpgrader(up *websocket.Upgrader) {
+	upgrader = up
+}
+
+type UpgradeConn struct {
+	conn   *websocket.Conn
+	authOK bool
+	authFn func(ctx context.Context, msg *WSMessage) (*WSMessage, error)
+}
+
+func (c *UpgradeConn) Read(ctx context.Context, handler func(ctx context.Context, msg *WSMessage) (*WSMessage, error)) error {
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Error("websocket read panic", zap.Any("error", err), zap.ByteString("stack", debug.Stack()))
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		t, b, err := c.conn.ReadMessage()
+		if err != nil {
+			return err
+		}
+
+		var msg *WSMessage
+
+		// if `authFunc` is not nil and unauthorized, need to authorize first.
+		if c.authFn != nil && !c.authOK {
+			msg, err = c.authFn(ctx, NewWSMessage(t, b))
+			if err != nil {
+				msg = WSTextMessage(err.Error())
+			} else {
+				c.authOK = true
+			}
+		} else {
+			if handler != nil {
+				msg, err = handler(ctx, NewWSMessage(t, b))
+				if err != nil {
+					msg = WSTextMessage(err.Error())
+				}
+			}
+		}
+
+		if msg != nil {
+			c.conn.WriteMessage(msg.T(), msg.V())
+		}
+	}
+}
+
+func (c *UpgradeConn) Write(ctx context.Context, msg *WSMessage) error {
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
@@ -112,28 +305,24 @@ func (c *wsconn) Write(ctx context.Context, msg *WSMessage) error {
 
 	// if `authFn` is not nil and unauthorized, disable to write message.
 	if c.authFn != nil && !c.authOK {
-		return errors.New("write msg disabled due to unauthorized")
+		return errors.New("write msg disabled becaule of unauthorized")
 	}
 
 	return c.conn.WriteMessage(msg.T(), msg.V())
 }
 
-func (c *wsconn) Close(ctx context.Context) error {
+func (c *UpgradeConn) Close() error {
 	return c.conn.Close()
 }
 
 // WSUpgrade upgrades the HTTP server connection to the WebSocket protocol.
-func WSUpgrade(w http.ResponseWriter, r *http.Request, authFn func(ctx context.Context, msg *WSMessage) (*WSMessage, error)) (WSConn, error) {
-	if upgrader == nil {
-		return nil, errors.New("upgrader is nil (forgotten configure?)")
-	}
-
+func WSUpgrade(w http.ResponseWriter, r *http.Request, authFn func(ctx context.Context, msg *WSMessage) (*WSMessage, error)) (*UpgradeConn, error) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	conn := &wsconn{
+	conn := &UpgradeConn{
 		conn:   c,
 		authFn: authFn,
 	}
