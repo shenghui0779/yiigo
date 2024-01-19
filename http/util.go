d6ddcc7ccae2914ec0c90b88e4f6f30bf900feb8
shenghui <shenghui0779@gmail.com>
2024-01-19 10:05:00 +0800

    Refactor HTTP utility functions

--- http/util.go
+++ http/util.go
@@ -24,34 +24,34 @@ const (
 
 var defaultCli = NewDefaultClient()
 
-// HTTPGet 发送GET请求
-func HTTPGet(ctx context.Context, reqURL string, options ...Option) (*http.Response, error) {
+// Get 发送GET请求
+func Get(ctx context.Context, reqURL string, options ...Option) (*http.Response, error) {
 	return defaultCli.Do(ctx, http.MethodGet, reqURL, nil, options...)
 }
 
-// HTTPPost 发送POST请求
-func HTTPPost(ctx context.Context, reqURL string, body []byte, options ...Option) (*http.Response, error) {
+// Post 发送POST请求
+func Post(ctx context.Context, reqURL string, body []byte, options ...Option) (*http.Response, error) {
 	return defaultCli.Do(ctx, http.MethodPost, reqURL, body, options...)
 }
 
-// HTTPPostJSON 发送POST请求(json数据)
-func HTTPPostJSON(ctx context.Context, reqURL string, body []byte, options ...Option) (*http.Response, error) {
+// PostJSON 发送POST请求(json数据)
+func PostJSON(ctx context.Context, reqURL string, body []byte, options ...Option) (*http.Response, error) {
 	options = append(options, WithHeader(HeaderContentType, ContentJSON))
 	return defaultCli.Do(ctx, http.MethodPost, reqURL, body, options...)
 }
 
-// HTTPPostForm 发送POST表单请求
-func HTTPPostForm(ctx context.Context, reqURL string, data url.Values, options ...Option) (*http.Response, error) {
+// PostForm 发送POST表单请求
+func PostForm(ctx context.Context, reqURL string, data url.Values, options ...Option) (*http.Response, error) {
 	options = append(options, WithHeader(HeaderContentType, ContentForm))
 	return defaultCli.Do(ctx, http.MethodPost, reqURL, []byte(data.Encode()), options...)
 }
 
-// HTTPUpload 文件上传
-func HTTPUpload(ctx context.Context, reqURL string, form UploadForm, options ...Option) (*http.Response, error) {
+// Upload 文件上传
+func Upload(ctx context.Context, reqURL string, form UploadForm, options ...Option) (*http.Response, error) {
 	return defaultCli.Upload(ctx, reqURL, form, options...)
 }
 
-// HTTPDo 发送HTTP请求
-func HTTPDo(ctx context.Context, method, reqURL string, body []byte, options ...Option) (*http.Response, error) {
+// Do 发送HTTP请求
+func Do(ctx context.Context, method, reqURL string, body []byte, options ...Option) (*http.Response, error) {
 	return defaultCli.Do(ctx, method, reqURL, body, options...)
 }
