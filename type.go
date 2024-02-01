6f030cd0e22ef3acce8890331935d6efdc7acbef
shenghui <shenghui0779@gmail.com>
2024-02-01 14:55:41 +0800

    Update type.go

--- type.go
+++ type.go
@@ -1,5 +1,7 @@
 package yiigo
 
+import "fmt"
+
 // X 类型别名
 type X map[string]any
 
@@ -10,4 +12,37 @@ const (
 	MySQL    DBDriver = "mysql"
 	Postgres DBDriver = "pgx"
 	SQLite   DBDriver = "sqlite3"
+)
+
+type Quantity int64
+
+func (q Quantity) String() string {
+	if q >= TiB {
+		return fmt.Sprintf("%.2fTB", float64(q)/float64(TiB))
+	}
+
+	if q >= GiB {
+		return fmt.Sprintf("%.2fGB", float64(q)/float64(GiB))
+	}
+
+	if q >= MiB {
+		return fmt.Sprintf("%.2fMB", float64(q)/float64(MiB))
+	}
+
+	if q >= KiB {
+		return fmt.Sprintf("%.2fKB", float64(q)/float64(KiB))
+	}
+
+	return fmt.Sprintf("%dB", q)
+}
+
+const (
+	// TiB - TebiByte size
+	TiB Quantity = 1024 * 1024 * 1024 * 1024
+	// GiB - GibiByte size
+	GiB Quantity = 1024 * 1024 * 1024
+	// MiB - MebiByte size
+	MiB Quantity = 1024 * 1024
+	// KiB - KibiByte size
+	KiB Quantity = 1024
 )
