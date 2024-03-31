87544d1916f5170f9d2b1060b881d184353a0687
shenghui <shenghui0779@gmail.com>
2024-03-31 21:21:35 +0800

    Update type.go

--- type.go
+++ type.go
@@ -5,17 +5,21 @@ import "fmt"
 // X 类型别名
 type X map[string]any
 
-// DBDriver 数据库驱动
-type DBDriver string
-
-const (
-	MySQL    DBDriver = "mysql"
-	Postgres DBDriver = "pgx"
-	SQLite   DBDriver = "sqlite3"
-)
-
 // Quantity 字节大小
 type Quantity int64
+
+const (
+	// B - Byte size
+	B Quantity = 1
+	// KiB - KibiByte size
+	KiB Quantity = 1024 * B
+	// MiB - MebiByte size
+	MiB Quantity = 1024 * KiB
+	// GiB - GibiByte size
+	GiB Quantity = 1024 * MiB
+	// TiB - TebiByte size
+	TiB Quantity = 1024 * GiB
+)
 
 // String 实现 Stringer 接口
 func (q Quantity) String() string {
@@ -37,16 +41,3 @@ func (q Quantity) String() string {
 
 	return fmt.Sprintf("%dB", q)
 }
-
-const (
-	// B - Byte size
-	B Quantity = 1
-	// KiB - KibiByte size
-	KiB Quantity = 1024 * B
-	// MiB - MebiByte size
-	MiB Quantity = 1024 * KiB
-	// GiB - GibiByte size
-	GiB Quantity = 1024 * MiB
-	// TiB - TebiByte size
-	TiB Quantity = 1024 * GiB
-)
