4b685e473a5ce39c773046c22518c1f4c95b2dff
shenghui0779 <shenghui0779@gmail.com>
2024-04-22 20:10:38 +0800

    Update mutex.go

--- mutex.go
+++ mutex.go
@@ -2,15 +2,19 @@ package yiigo
 
 import (
 	"context"
+	"math/rand"
 	"time"
 
+	"github.com/jaevor/go-nanoid"
 	"github.com/redis/go-redis/v9"
 )
 
-// Mutex 分布式锁
-type Mutex interface {
-	// Lock 尝试获取锁；interval - 每隔指定时间尝试获取一次锁；timeout - 获取锁的超时时间
-	Lock(ctx context.Context, interval, timeout time.Duration) error
+// DistributedMutex 分布式锁
+type DistributedMutex interface {
+	// Lock 获取锁
+	Lock(ctx context.Context) (bool, error)
+	// TryLock 尝试获取锁
+	TryLock(ctx context.Context, attempts int) (bool, error)
 	// UnLock 释放锁
 	UnLock(ctx context.Context) error
 }
@@ -19,54 +23,93 @@ type Mutex interface {
 type distributed struct {
 	cli    *redis.Client
 	key    string
-	uniqID string
+	token  string
 	expire time.Duration
 }
 
-func (d *distributed) Lock(ctx context.Context, interval, timeout time.Duration) error {
-	ctx, cancel := context.WithTimeout(ctx, timeout)
-	defer cancel()
+func (d *distributed) Lock(ctx context.Context) (bool, error) {
+	select {
+	case <-ctx.Done(): // timeout or canceled
+		return false, ctx.Err()
+	default:
+	}
 
-	for {
+	if err := d.lock(ctx); err != nil {
+		return false, err
+	}
+
+	return len(d.token) != 0, nil
+}
+
+func (d *distributed) TryLock(ctx context.Context, attempts int) (bool, error) {
+	for i := 0; i < attempts; i++ {
 		select {
 		case <-ctx.Done(): // timeout or canceled
-			return ctx.Err()
+			return false, ctx.Err()
 		default:
 		}
 
-		// attempt to acquire lock with `setnx`
-		ok, err := d.cli.SetNX(ctx, d.key, d.uniqID, d.expire).Result()
-		if err != nil {
-			return err
+		// attempt to acquire lock
+		if err := d.lock(ctx); err != nil {
+			return false, err
 		}
-		if ok {
-			return nil
+		if len(d.token) != 0 {
+			return true, nil
 		}
-
-		time.Sleep(interval)
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(150)+50))
 	}
+
+	return false, nil
 }
 
 func (d *distributed) UnLock(ctx context.Context) error {
+	if len(d.token) == 0 {
+		return nil
+	}
+
+	ctx = DetachContext(ctx)
+
 	v, err := d.cli.Get(ctx, d.key).Result()
 	if err != nil {
 		return err
 	}
-	if v != d.uniqID {
+	if v != d.token {
 		return nil
 	}
 
 	return d.cli.Del(ctx, d.key).Err()
 }
 
-// DistributedMutex 返回一个分布式锁实例；
-// uniqID - 建议使用「RequestID」
-func DistributedMutex(cli *redis.Client, key, uniqID string, expire time.Duration) Mutex {
+func (d *distributed) lock(ctx context.Context) error {
+	uniqID, _ := nanoid.Standard(32)
+	token := uniqID()
+	ok, err := d.cli.SetNX(ctx, d.key, token, d.expire).Result()
+	if err != nil {
+		// 尝试GET一次：避免因redis网络错误导致误加锁
+		v, _err := d.cli.Get(ctx, d.key).Result()
+		if _err != nil {
+			if _err == redis.Nil {
+				return err
+			}
+			return _err
+		}
+		if v == token {
+			d.token = token
+		}
+		return nil
+	}
+	if ok {
+		d.token = token
+	}
+	return nil
+}
+
+// RedisMutex 基于Redis实现的分布式锁实例
+func RedisMutex(cli *redis.Client, key string, ttl time.Duration) DistributedMutex {
 	mutex := &distributed{
 		cli:    cli,
 		key:    key,
-		uniqID: uniqID,
-		expire: expire,
+		expire: ttl,
 	}
 
 	if mutex.expire == 0 {
