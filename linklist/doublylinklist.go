9a77d501b860d96406ca859e01160ad26bfbb548
shenghui0779 <shenghui0779@gmail.com>
2024-11-13 14:45:59 +0800

    Add linklist

---
+++ linklist/doublylinklist.go
@@ -0,0 +1,391 @@
+package linklist
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+	"sync"
+)
+
+// DoublyLinkList 双向链表(并发安全)
+type DoublyLinkList[T comparable] struct {
+	first *element[T]
+	last  *element[T]
+	size  int
+	mutex sync.RWMutex
+}
+
+type element[T comparable] struct {
+	value T
+	prev  *element[T]
+	next  *element[T]
+}
+
+// New 初始化一个链表
+func New[T comparable](values ...T) *DoublyLinkList[T] {
+	list := &DoublyLinkList[T]{}
+	if len(values) > 0 {
+		list.Append(values...)
+	}
+	return list
+}
+
+// Append 向链表尾部追加值
+func (list *DoublyLinkList[T]) Append(values ...T) {
+	list.mutex.Lock()
+	defer list.mutex.Unlock()
+
+	for _, value := range values {
+		newElement := &element[T]{value: value, prev: list.last}
+		if list.size == 0 {
+			list.first = newElement
+			list.last = newElement
+		} else {
+			list.last.next = newElement
+			list.last = newElement
+		}
+		list.size++
+	}
+}
+
+// Prepend 向链表头部追加值
+func (list *DoublyLinkList[T]) Prepend(values ...T) {
+	list.mutex.Lock()
+	defer list.mutex.Unlock()
+
+	// in reverse to keep passed order i.e. ["c","d"] -> Prepend(["a","b"]) -> ["a","b","c",d"]
+	for v := len(values) - 1; v >= 0; v-- {
+		newElement := &element[T]{value: values[v], next: list.first}
+		if list.size == 0 {
+			list.first = newElement
+			list.last = newElement
+		} else {
+			list.first.prev = newElement
+			list.first = newElement
+		}
+		list.size++
+	}
+}
+
+// Get 获取指定索引位置元素的值
+func (list *DoublyLinkList[T]) Get(index int) (T, bool) {
+	list.mutex.RLock()
+	defer list.mutex.RUnlock()
+
+	if !list.withinRange(index) {
+		var t T
+		return t, false
+	}
+
+	e := list.getElement(index)
+	return e.value, true
+}
+
+// Remove 移除指定索引位置的元素并返回该元素的值
+func (list *DoublyLinkList[T]) Remove(index int) (T, bool) {
+	list.mutex.Lock()
+	defer list.mutex.Unlock()
+
+	if !list.withinRange(index) {
+		var t T
+		return t, false
+	}
+
+	e := list.getElement(index)
+	value := e.value
+	list.deleteElement(e)
+
+	return value, true
+}
+
+// Each 遍历操作
+func (list *DoublyLinkList[T]) Each(fn func(index int, value T)) {
+	list.mutex.RLock()
+	defer list.mutex.RUnlock()
+
+	for i, e := 0, list.first; e != nil; i, e = i+1, e.next {
+		fn(i, e.value)
+	}
+}
+
+// Map 根据指定条件，返回一个新链表
+func (list *DoublyLinkList[T]) Map(fn func(index int, value T) T) *DoublyLinkList[T] {
+	list.mutex.RLock()
+	defer list.mutex.RUnlock()
+
+	newList := &DoublyLinkList[T]{}
+	for i, e := 0, list.first; e != nil; i, e = i+1, e.next {
+		newList.Append(fn(i, e.value))
+	}
+	return newList
+}
+
+// Filter 过滤出指定条件的元素，移除它们并返回对应的值
+func (list *DoublyLinkList[T]) Filter(fn func(index int, value T) bool) []T {
+	list.mutex.Lock()
+	defer list.mutex.Unlock()
+
+	var elements []*element[T]
+	for i, e := 0, list.first; e != nil; i, e = i+1, e.next {
+		if fn(i, e.value) {
+			elements = append(elements, e)
+		}
+	}
+
+	values := make([]T, 0, len(elements))
+	for _, e := range elements {
+		values = append(values, e.value)
+		list.deleteElement(e)
+	}
+
+	return values
+}
+
+// Contains 返回链表中是否包含指定值中的一个
+func (list *DoublyLinkList[T]) Contains(values ...T) bool {
+	list.mutex.RLock()
+	defer list.mutex.RUnlock()
+
+	if len(values) == 0 {
+		return true
+	}
+	if list.size == 0 {
+		return false
+	}
+	for _, value := range values {
+		found := false
+		for e := list.first; e != nil; e = e.next {
+			if e.value == value {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+// Values 返回链表所有元素的值
+func (list *DoublyLinkList[T]) Values() []T {
+	list.mutex.RLock()
+	defer list.mutex.RUnlock()
+
+	values := make([]T, 0, list.size)
+	for e := list.first; e != nil; e = e.next {
+		values = append(values, e.value)
+	}
+	return values
+}
+
+// IndexOf 返回指定值在链表中的索引，-1表示不存在
+func (list *DoublyLinkList[T]) IndexOf(value T) int {
+	list.mutex.RLock()
+	defer list.mutex.RUnlock()
+
+	if list.size == 0 {
+		return -1
+	}
+	for index, e := range list.Values() {
+		if e == value {
+			return index
+		}
+	}
+	return -1
+}
+
+// Empty 返回链表是否为空
+func (list *DoublyLinkList[T]) Empty() bool {
+	return list.size == 0
+}
+
+// Size 返回链表的大小
+func (list *DoublyLinkList[T]) Size() int {
+	return list.size
+}
+
+// Clear 清空链表
+func (list *DoublyLinkList[T]) Clear() {
+	list.mutex.Lock()
+	defer list.mutex.Unlock()
+
+	list.size = 0
+	list.first = nil
+	list.last = nil
+}
+
+// Sort 将链表中元素值排序
+func (list *DoublyLinkList[T]) Sort(comparator func(x, y T) int) {
+	list.mutex.Lock()
+	defer list.mutex.Unlock()
+
+	if list.size < 2 {
+		return
+	}
+
+	values := list.Values()
+	slices.SortFunc(values, comparator)
+
+	list.Clear()
+	list.Append(values...)
+}
+
+// Swap 交换链表中两个指定索引的元素
+func (list *DoublyLinkList[T]) Swap(i, j int) {
+	list.mutex.Lock()
+	defer list.mutex.Unlock()
+
+	if list.withinRange(i) && list.withinRange(j) && i != j {
+		var element1, element2 *element[T]
+		for e, currentElement := 0, list.first; element1 == nil || element2 == nil; e, currentElement = e+1, currentElement.next {
+			switch e {
+			case i:
+				element1 = currentElement
+			case j:
+				element2 = currentElement
+			}
+		}
+		element1.value, element2.value = element2.value, element1.value
+	}
+}
+
+// Insert 在链表的指定索引位置插入值
+func (list *DoublyLinkList[T]) Insert(index int, values ...T) {
+	list.mutex.Lock()
+	defer list.mutex.Unlock()
+
+	if !list.withinRange(index) {
+		// Append
+		if index == list.size {
+			list.Append(values...)
+		}
+		return
+	}
+
+	var beforeElement *element[T]
+	var foundElement *element[T]
+	// determine traversal direction, last to first or first to last
+	if list.size-index < index {
+		foundElement = list.last
+		beforeElement = list.last.prev
+		for e := list.size - 1; e != index; e, foundElement = e-1, foundElement.prev {
+			beforeElement = beforeElement.prev
+		}
+	} else {
+		foundElement = list.first
+		for e := 0; e != index; e, foundElement = e+1, foundElement.next {
+			beforeElement = foundElement
+		}
+	}
+
+	if foundElement == list.first {
+		oldNextElement := list.first
+		for i, value := range values {
+			newElement := &element[T]{value: value}
+			if i == 0 {
+				list.first = newElement
+			} else {
+				newElement.prev = beforeElement
+				beforeElement.next = newElement
+			}
+			beforeElement = newElement
+		}
+		oldNextElement.prev = beforeElement
+		beforeElement.next = oldNextElement
+	} else {
+		oldNextElement := beforeElement.next
+		for _, value := range values {
+			newElement := &element[T]{value: value}
+			newElement.prev = beforeElement
+			beforeElement.next = newElement
+			beforeElement = newElement
+		}
+		oldNextElement.prev = beforeElement
+		beforeElement.next = oldNextElement
+	}
+
+	list.size += len(values)
+}
+
+// Set 设置链表指定索引位置索引的值
+func (list *DoublyLinkList[T]) Set(index int, value T) {
+	list.mutex.Lock()
+	defer list.mutex.Unlock()
+
+	if !list.withinRange(index) {
+		// Append
+		if index == list.size {
+			list.Append(value)
+		}
+		return
+	}
+
+	var foundElement *element[T]
+	// determine traversal direction, last to first or first to last
+	if list.size-index < index {
+		foundElement = list.last
+		for e := list.size - 1; e != index; {
+			fmt.Println("Set last", index, value, foundElement, foundElement.prev)
+			e, foundElement = e-1, foundElement.prev
+		}
+	} else {
+		foundElement = list.first
+		for e := 0; e != index; {
+			e, foundElement = e+1, foundElement.next
+		}
+	}
+
+	foundElement.value = value
+}
+
+// String 实现 Stringer Interface
+func (list *DoublyLinkList[T]) String() string {
+	list.mutex.RLock()
+	defer list.mutex.RUnlock()
+
+	str := "DoublyLinkList\n"
+	var values []string
+	for e := list.first; e != nil; e = e.next {
+		values = append(values, fmt.Sprintf("%v", e.value))
+	}
+	str += strings.Join(values, ", ")
+	return str
+}
+
+func (list *DoublyLinkList[T]) withinRange(index int) bool {
+	return index >= 0 && index < list.size
+}
+
+func (list *DoublyLinkList[T]) getElement(index int) *element[T] {
+	// determine traveral direction, last to first or first to last
+	if list.size-index < index {
+		e := list.last
+		for i := list.size - 1; i != index; i, e = i-1, e.prev {
+		}
+		return e
+	}
+
+	e := list.first
+	for i := 0; i != index; i, e = i+1, e.next {
+	}
+	return e
+}
+
+func (list *DoublyLinkList[T]) deleteElement(e *element[T]) {
+	if e == list.first {
+		list.first = e.next
+	}
+	if e == list.last {
+		list.last = e.prev
+	}
+	if e.prev != nil {
+		e.prev.next = e.next
+	}
+	if e.next != nil {
+		e.next.prev = e.prev
+	}
+	e = nil
+
+	list.size--
+}
---
+++ linklist/doublylinklist_test.go
@@ -0,0 +1,464 @@
+package linklist
+
+import (
+	"cmp"
+	"slices"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	list1 := New[int]()
+
+	if actualValue := list1.Empty(); actualValue != true {
+		t.Errorf("Got %v expected %v", actualValue, true)
+	}
+
+	list2 := New[int](1, 2)
+
+	if actualValue := list2.Size(); actualValue != 2 {
+		t.Errorf("Got %v expected %v", actualValue, 2)
+	}
+
+	if actualValue, ok := list2.Get(0); actualValue != 1 || !ok {
+		t.Errorf("Got %v expected %v", actualValue, 1)
+	}
+
+	if actualValue, ok := list2.Get(1); actualValue != 2 || !ok {
+		t.Errorf("Got %v expected %v", actualValue, 2)
+	}
+
+	if actualValue, ok := list2.Get(2); actualValue != 0 || ok {
+		t.Errorf("Got %v expected %v", actualValue, 0)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	list := New[string]()
+	list.Append("a")
+	list.Append("b", "c")
+	if actualValue := list.Empty(); actualValue != false {
+		t.Errorf("Got %v expected %v", actualValue, false)
+	}
+	if actualValue := list.Size(); actualValue != 3 {
+		t.Errorf("Got %v expected %v", actualValue, 3)
+	}
+	if actualValue, ok := list.Get(2); actualValue != "c" || !ok {
+		t.Errorf("Got %v expected %v", actualValue, "c")
+	}
+}
+
+func TestAppendAndPrepend(t *testing.T) {
+	list := New[string]()
+	list.Append("b")
+	list.Prepend("a")
+	list.Append("c")
+	if actualValue := list.Empty(); actualValue != false {
+		t.Errorf("Got %v expected %v", actualValue, false)
+	}
+	if actualValue := list.Size(); actualValue != 3 {
+		t.Errorf("Got %v expected %v", actualValue, 3)
+	}
+	if actualValue, ok := list.Get(0); actualValue != "a" || !ok {
+		t.Errorf("Got %v expected %v", actualValue, "c")
+	}
+	if actualValue, ok := list.Get(1); actualValue != "b" || !ok {
+		t.Errorf("Got %v expected %v", actualValue, "c")
+	}
+	if actualValue, ok := list.Get(2); actualValue != "c" || !ok {
+		t.Errorf("Got %v expected %v", actualValue, "c")
+	}
+}
+
+func TestRemove(t *testing.T) {
+	list := New[string]()
+	list.Append("a")
+	list.Append("b", "c")
+	list.Remove(2)
+	if actualValue, ok := list.Get(2); actualValue != "" || ok {
+		t.Errorf("Got %v expected %v", actualValue, "")
+	}
+	list.Remove(1)
+	list.Remove(0)
+	list.Remove(0) // no effect
+	if actualValue := list.Empty(); actualValue != true {
+		t.Errorf("Got %v expected %v", actualValue, true)
+	}
+	if actualValue := list.Size(); actualValue != 0 {
+		t.Errorf("Got %v expected %v", actualValue, 0)
+	}
+}
+
+func TestGet(t *testing.T) {
+	list := New[string]()
+	list.Append("a")
+	list.Append("b", "c")
+	if actualValue, ok := list.Get(0); actualValue != "a" || !ok {
+		t.Errorf("Got %v expected %v", actualValue, "a")
+	}
+	if actualValue, ok := list.Get(1); actualValue != "b" || !ok {
+		t.Errorf("Got %v expected %v", actualValue, "b")
+	}
+	if actualValue, ok := list.Get(2); actualValue != "c" || !ok {
+		t.Errorf("Got %v expected %v", actualValue, "c")
+	}
+	if actualValue, ok := list.Get(3); actualValue != "" || ok {
+		t.Errorf("Got %v expected %v", actualValue, "")
+	}
+	list.Remove(0)
+	if actualValue, ok := list.Get(0); actualValue != "b" || !ok {
+		t.Errorf("Got %v expected %v", actualValue, "b")
+	}
+}
+
+func TestPop(t *testing.T) {
+	list := New[string]()
+	list.Append("a", "b", "c")
+	v, ok := list.Remove(0)
+	assert.True(t, ok)
+	assert.Equal(t, "a", v)
+	assert.Equal(t, 2, list.Size())
+	v, ok = list.Remove(0)
+	assert.True(t, ok)
+	assert.Equal(t, "b", v)
+	assert.Equal(t, 1, list.Size())
+	v, ok = list.Remove(0)
+	assert.True(t, ok)
+	assert.Equal(t, "c", v)
+	assert.Equal(t, 0, list.Size())
+}
+
+func TestEach(t *testing.T) {
+	list := New[string]()
+	list.Append("a", "b", "c")
+	list.Each(func(index int, value string) {
+		switch index {
+		case 0:
+			if actualValue, expectedValue := value, "a"; actualValue != expectedValue {
+				t.Errorf("Got %v expected %v", actualValue, expectedValue)
+			}
+		case 1:
+			if actualValue, expectedValue := value, "b"; actualValue != expectedValue {
+				t.Errorf("Got %v expected %v", actualValue, expectedValue)
+			}
+		case 2:
+			if actualValue, expectedValue := value, "c"; actualValue != expectedValue {
+				t.Errorf("Got %v expected %v", actualValue, expectedValue)
+			}
+		default:
+			t.Errorf("Too many")
+		}
+	})
+}
+
+func TestMap(t *testing.T) {
+	list := New[string]()
+	list.Append("a", "b", "c")
+	mappedList := list.Map(func(index int, value string) string {
+		return "mapped: " + value
+	})
+	if actualValue, _ := mappedList.Get(0); actualValue != "mapped: a" {
+		t.Errorf("Got %v expected %v", actualValue, "mapped: a")
+	}
+	if actualValue, _ := mappedList.Get(1); actualValue != "mapped: b" {
+		t.Errorf("Got %v expected %v", actualValue, "mapped: b")
+	}
+	if actualValue, _ := mappedList.Get(2); actualValue != "mapped: c" {
+		t.Errorf("Got %v expected %v", actualValue, "mapped: c")
+	}
+	if mappedList.Size() != 3 {
+		t.Errorf("Got %v expected %v", mappedList.Size(), 3)
+	}
+}
+
+func TestFilter(t *testing.T) {
+	list := New[string]()
+	list.Append("a", "b", "c", "d", "e")
+	values := list.Filter(func(index int, value string) bool {
+		if value == "b" || value == "d" {
+			return true
+		}
+		return false
+	})
+	assert.Equal(t, []string{"b", "d"}, values)
+	assert.Equal(t, 3, list.Size())
+	assert.Equal(t, []string{"a", "c", "e"}, list.Values())
+}
+
+func TestSwap(t *testing.T) {
+	list := New[string]()
+	list.Append("a")
+	list.Append("b", "c")
+	list.Swap(0, 1)
+	if actualValue, ok := list.Get(0); actualValue != "b" || !ok {
+		t.Errorf("Got %v expected %v", actualValue, "b")
+	}
+}
+
+func TestSort(t *testing.T) {
+	list := New[string]()
+	list.Sort(cmp.Compare[string])
+	list.Append("e", "f", "g", "a", "b", "c", "d")
+	list.Sort(cmp.Compare[string])
+	for i := 1; i < list.Size(); i++ {
+		a, _ := list.Get(i - 1)
+		b, _ := list.Get(i)
+		if a > b {
+			t.Errorf("Not sorted! %s > %s", a, b)
+		}
+	}
+}
+
+func TestClear(t *testing.T) {
+	list := New[string]()
+	list.Append("e", "f", "g", "a", "b", "c", "d")
+	list.Clear()
+	if actualValue := list.Empty(); actualValue != true {
+		t.Errorf("Got %v expected %v", actualValue, true)
+	}
+	if actualValue := list.Size(); actualValue != 0 {
+		t.Errorf("Got %v expected %v", actualValue, 0)
+	}
+}
+
+func TestContains(t *testing.T) {
+	list := New[string]()
+	list.Append("a")
+	list.Append("b", "c")
+	if actualValue := list.Contains("a"); actualValue != true {
+		t.Errorf("Got %v expected %v", actualValue, true)
+	}
+	if actualValue := list.Contains(""); actualValue != false {
+		t.Errorf("Got %v expected %v", actualValue, false)
+	}
+	if actualValue := list.Contains("a", "b", "c"); actualValue != true {
+		t.Errorf("Got %v expected %v", actualValue, true)
+	}
+	if actualValue := list.Contains("a", "b", "c", "d"); actualValue != false {
+		t.Errorf("Got %v expected %v", actualValue, false)
+	}
+	list.Clear()
+	if actualValue := list.Contains("a"); actualValue != false {
+		t.Errorf("Got %v expected %v", actualValue, false)
+	}
+	if actualValue := list.Contains("a", "b", "c"); actualValue != false {
+		t.Errorf("Got %v expected %v", actualValue, false)
+	}
+}
+
+func TestValues(t *testing.T) {
+	list := New[string]()
+	list.Append("a")
+	list.Append("b", "c")
+	if actualValue, expectedValue := list.Values(), []string{"a", "b", "c"}; !slices.Equal(actualValue, expectedValue) {
+		t.Errorf("Got %v expected %v", actualValue, expectedValue)
+	}
+}
+
+func TestInsert(t *testing.T) {
+	list := New[string]()
+	list.Insert(0, "b", "c", "d")
+	list.Insert(0, "a")
+	list.Insert(10, "x") // ignore
+	if actualValue := list.Size(); actualValue != 4 {
+		t.Errorf("Got %v expected %v", actualValue, 4)
+	}
+	list.Insert(4, "g") // append
+	if actualValue := list.Size(); actualValue != 5 {
+		t.Errorf("Got %v expected %v", actualValue, 5)
+	}
+	if actualValue, expectedValue := strings.Join(list.Values(), ""), "abcdg"; actualValue != expectedValue {
+		t.Errorf("Got %v expected %v", actualValue, expectedValue)
+	}
+	list.Insert(4, "e", "f") // last to first traversal
+	if actualValue := list.Size(); actualValue != 7 {
+		t.Errorf("Got %v expected %v", actualValue, 7)
+	}
+	if actualValue, expectedValue := strings.Join(list.Values(), ""), "abcdefg"; actualValue != expectedValue {
+		t.Errorf("Got %v expected %v", actualValue, expectedValue)
+	}
+}
+
+func TestSet(t *testing.T) {
+	list := New[string]()
+	list.Set(0, "a")
+	list.Set(1, "b")
+	if actualValue := list.Size(); actualValue != 2 {
+		t.Errorf("Got %v expected %v", actualValue, 2)
+	}
+	list.Set(2, "c") // append
+	if actualValue := list.Size(); actualValue != 3 {
+		t.Errorf("Got %v expected %v", actualValue, 3)
+	}
+	list.Set(4, "d")  // ignore
+	list.Set(1, "bb") // update
+	if actualValue := list.Size(); actualValue != 3 {
+		t.Errorf("Got %v expected %v", actualValue, 3)
+	}
+	if actualValue, expectedValue := list.Values(), []string{"a", "bb", "c"}; !slices.Equal(actualValue, expectedValue) {
+		t.Errorf("Got %v expected %v", actualValue, expectedValue)
+	}
+}
+
+func TestString(t *testing.T) {
+	c := New[int]()
+	c.Append(1)
+	if !strings.HasPrefix(c.String(), "DoublyLinkedList") {
+		t.Errorf("String should start with container name")
+	}
+}
+
+func benchmarkGet(b *testing.B, list *DoublyLinkList[int], size int) {
+	for i := 0; i < b.N; i++ {
+		for n := 0; n < size; n++ {
+			list.Get(n)
+		}
+	}
+}
+
+func benchmarkAdd(b *testing.B, list *DoublyLinkList[int], size int) {
+	for i := 0; i < b.N; i++ {
+		for n := 0; n < size; n++ {
+			list.Append(n)
+		}
+	}
+}
+
+func benchmarkRemove(b *testing.B, list *DoublyLinkList[int], size int) {
+	for i := 0; i < b.N; i++ {
+		for n := 0; n < size; n++ {
+			list.Remove(n)
+		}
+	}
+}
+
+func BenchmarkDoublyLinkedListGet100(b *testing.B) {
+	b.StopTimer()
+	size := 100
+	list := New[int]()
+	for n := 0; n < size; n++ {
+		list.Append(n)
+	}
+	b.StartTimer()
+	benchmarkGet(b, list, size)
+}
+
+func BenchmarkDoublyLinkedListGet1000(b *testing.B) {
+	b.StopTimer()
+	size := 1000
+	list := New[int]()
+	for n := 0; n < size; n++ {
+		list.Append(n)
+	}
+	b.StartTimer()
+	benchmarkGet(b, list, size)
+}
+
+func BenchmarkDoublyLinkedListGet10000(b *testing.B) {
+	b.StopTimer()
+	size := 10000
+	list := New[int]()
+	for n := 0; n < size; n++ {
+		list.Append(n)
+	}
+	b.StartTimer()
+	benchmarkGet(b, list, size)
+}
+
+func BenchmarkDoublyLinkedListGet100000(b *testing.B) {
+	b.StopTimer()
+	size := 100000
+	list := New[int]()
+	for n := 0; n < size; n++ {
+		list.Append(n)
+	}
+	b.StartTimer()
+	benchmarkGet(b, list, size)
+}
+
+func BenchmarkDoublyLinkedListAdd100(b *testing.B) {
+	b.StopTimer()
+	size := 100
+	list := New[int]()
+	b.StartTimer()
+	benchmarkAdd(b, list, size)
+}
+
+func BenchmarkDoublyLinkedListAdd1000(b *testing.B) {
+	b.StopTimer()
+	size := 1000
+	list := New[int]()
+	for n := 0; n < size; n++ {
+		list.Append(n)
+	}
+	b.StartTimer()
+	benchmarkAdd(b, list, size)
+}
+
+func BenchmarkDoublyLinkedListAdd10000(b *testing.B) {
+	b.StopTimer()
+	size := 10000
+	list := New[int]()
+	for n := 0; n < size; n++ {
+		list.Append(n)
+	}
+	b.StartTimer()
+	benchmarkAdd(b, list, size)
+}
+
+func BenchmarkDoublyLinkedListAdd100000(b *testing.B) {
+	b.StopTimer()
+	size := 100000
+	list := New[int]()
+	for n := 0; n < size; n++ {
+		list.Append(n)
+	}
+	b.StartTimer()
+	benchmarkAdd(b, list, size)
+}
+
+func BenchmarkDoublyLinkedListRemove100(b *testing.B) {
+	b.StopTimer()
+	size := 100
+	list := New[int]()
+	for n := 0; n < size; n++ {
+		list.Append(n)
+	}
+	b.StartTimer()
+	benchmarkRemove(b, list, size)
+}
+
+func BenchmarkDoublyLinkedListRemove1000(b *testing.B) {
+	b.StopTimer()
+	size := 1000
+	list := New[int]()
+	for n := 0; n < size; n++ {
+		list.Append(n)
+	}
+	b.StartTimer()
+	benchmarkRemove(b, list, size)
+}
+
+func BenchmarkDoublyLinkedListRemove10000(b *testing.B) {
+	b.StopTimer()
+	size := 10000
+	list := New[int]()
+	for n := 0; n < size; n++ {
+		list.Append(n)
+	}
+	b.StartTimer()
+	benchmarkRemove(b, list, size)
+}
+
+func BenchmarkDoublyLinkedListRemove100000(b *testing.B) {
+	b.StopTimer()
+	size := 100000
+	list := New[int]()
+	for n := 0; n < size; n++ {
+		list.Append(n)
+	}
+	b.StartTimer()
+	benchmarkRemove(b, list, size)
+}
