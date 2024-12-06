a58bc5049f5347ccfbdbbc7d01e097c3b44179b6
shenghui0779 <shenghui0779@gmail.com>
2024-12-06 11:01:05 +0800

    更新util.go

--- util.go
+++ util.go
@@ -64,9 +64,10 @@ func VersionCompare(rangeVer, curVer string) (bool, error) {
 
 // Retry 重试
 func Retry(ctx context.Context, fn func(ctx context.Context) error, attempts int, sleep time.Duration) (err error) {
+	threshold := attempts - 1
 	for i := 0; i < attempts; i++ {
 		err = fn(ctx)
-		if err == nil || i >= attempts-1 {
+		if err == nil || i >= threshold {
 			return
 		}
 		time.Sleep(sleep)
